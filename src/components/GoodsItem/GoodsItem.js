4eba96a9de3120977d70975e4456764f17f7abe1
AlHobb <118611357+alkopotov@users.noreply.github.com>
2023-10-03 14:34:28 +0300

    Ver 1.1.1

--- src/components/GoodsItem/GoodsItem.js
+++ src/components/GoodsItem/GoodsItem.js
@@ -4,20 +4,14 @@ function GoodsItem(props){
 
   const {id, name, price, count, incrementCount, decrementCount, deleteGood} = props
 
-  function handler(e) {
-    e.stopPropagation()
-    // console.log(e.currentTarget.id)
-    deleteGood(+e.target.id)
-  }
-
   return(
-    <div id={id} className={styles.good_item} onDoubleClick={handler}>
+    <div className={styles.good_item} onDoubleClick={(e) => deleteGood(id)}>
       <h2>{name}</h2>
       <h3>Цена: {price}</h3>
       <div className={styles.counter}>
-        <button className={styles.down_button} onClick={() => decrementCount(id)}>-</button>
+        <button className={styles.down_button} onClick={(e) => decrementCount(id)} onDoubleClick={(e) => e.stopPropagation()}>-</button>
         <h4>{count}</h4>
-        <button className={styles.up_button} onClick={() => incrementCount(id)}>+</button>
+        <button className={styles.up_button} onClick={() => incrementCount(id)} onDoubleClick={(e) => e.stopPropagation()}>+</button>
       </div>
     </div>
   )
